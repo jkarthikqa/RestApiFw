336d788eb7f21e5f2660c4fc00602c727e066246
Karthikeyan <jkarthikqa@yahoo.com>
2021-01-29 11:34:20 +0530

    Modidifind on Test Runner file

--- src/test/java/cocumber/Options/TestRunner.java
+++ src/test/java/cocumber/Options/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/java/features",
         glue = {"stepDefinations"},
         plugin = "json:target/jsonReports/cucumber-report.json",
-        tags = "@AddPlace"
+        tags = "@AddPlace or @DeletePlace"
 )
 public class TestRunner {
 }
